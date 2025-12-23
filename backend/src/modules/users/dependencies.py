7b8241b474140e8f8191f8ad4f2306c7867039bf
lmoura2 <lmoura2@lenovo.com>
2025-12-23 15:49:13 -0400

    fix: ajust to the user service logic

--- backend/src/modules/users/dependencies.py
+++ backend/src/modules/users/dependencies.py
@@ -3,12 +3,11 @@ from typing import Annotated
 from fastapi import Depends
 
 from ...shared.dependencies.database import Session
-from ...shared.dependencies.session_user import SessionUser
 from .service import UserService
 
 
-def get_service(user_login: SessionUser, session: Session):
-    return UserService(user_login, session)
+def get_service(session: Session):
+    return UserService(session)
 
 
 Service = Annotated[UserService, Depends(get_service)]
--- backend/src/modules/users/service.py
+++ backend/src/modules/users/service.py
@@ -2,37 +2,39 @@ from sqlalchemy import select
 
 from ...shared.dependencies.database import Session
 from ...shared.security.token_jwt import get_password_hash
-from ...shared.utils.exceptions import InvalidInputException
+from ...shared.utils.exceptions import UnauthorizedException
 from .model import User
-from .schema import UserCreateContract, UserCreateManualContract
+from .schema import UserCreateManualContract, UserUpdateContract
 
 
 class UserService:
-    def __init__(self, user: User, session: Session):
-        self.user = user
+    def __init__(self, session: Session):
         self.session = session
 
-    async def verify_user(self, email):
+    async def verify_user_by_email(self, email):
         user = self.session.scalar(select(User).where(User.email == email))
 
         return user
 
-    async def update_user(self, data: UserCreateContract) -> User:
+    async def update_user(self, user: User, data: UserUpdateContract):
         updated_user = data.model_dump(exclude_unset=True)
 
+        if user.email != data.email:
+            if self.verify_user_by_email(data.email):
+                raise UnauthorizedException()
+
         for key, value in updated_user.items():
-            setattr(self.user, key, value)
+            if key == "password":
+                value = get_password_hash(value)
+            setattr(user, key, value)
 
-        self.session.add(self.user)
+        self.session.add(user)
         await self.session.commit()
-        await self.session.refresh(self.user)
+        await self.session.refresh(user)
 
-        return self.user
+        return user
 
     async def create_user(self, data: UserCreateManualContract) -> User:
-        if await self.session.scalar(select(User).where(User.email == data.email)):
-            raise InvalidInputException()
-
         new_user = User(
             firstname=data.firstname,
             lastname=data.lastname,
@@ -40,6 +42,7 @@ class UserService:
             google_id=data.google_id,
             password=get_password_hash(data.password),
         )
+
         self.session.add(new_user)
         await self.session.commit()
         await self.session.refresh(new_user)
